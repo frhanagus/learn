1b60bf61e74092b10659a9834c598481ffd8d093
hanagus <farhanagusfer@gmail.com>
2024-03-11 16:33:34 +0700

    fixed using int

--- Python/BC Course/Day 1/teamipo2-RPS.py
+++ Python/BC Course/Day 1/teamipo2-RPS.py
@@ -1,25 +1,25 @@
 import random
 
-me = input("Choose 1)Rock, 2)Paper, or 3)Scissors: ").lower()
-comp = random.randint(1,3)
+me = int(input("Choose 1)Rock, 2)Paper, or 3)Scissors: ").lower())
+comp = int(random.randint(1,3))
 
-if me == "1":
+if me == int("1"):
     print("You chose Rock")
-elif me == "2":
+elif me == int("2"):
     print("You chose Paper")
 else : print("You chose Scissors")
 
-if comp == "1":
+if comp == int("1"):
     print("Computer chose Rock")
-elif comp == "2":
+elif comp == int("2"):
     print("Computer chose Paper")
 else : print("Computer chose Scissors")
 
 if me == comp:
     print("It's a tie!")
-elif (me == "1" and comp == "3") or \
-   (me == "2" and comp == "1") or \
-   (me == "3" and comp == "2"):
+elif (me == int("1") and comp == int("3")) or \
+   (me == int("2") and comp == int("1")) or \
+   (me == int("3") and comp == int("2")):
     print("You win!")
 else:
     print("You lose!")
